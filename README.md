f9d38d9effacb819692ce61f05f9b23284ee1872
hccake <chengbohua@foxmail.com>
2021-04-15 18:42:34 +0800

    :bookmark: release v0.0.9

---
+++ README.md
@@ -0,0 +1,4 @@
+# BallCat-Codegen
+
+- 预览地址：http://codegen.ballcat.cn/
+- 文档地址：http://www.ballcat.cn/codegen/
---
+++ doc/1schema.sql
@@ -0,0 +1,2 @@
+-- 代码生成器
+create database `ballcat_codegen` default character set utf8mb4 collate utf8mb4_general_ci;
