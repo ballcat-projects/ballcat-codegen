6b794bdc44640f1ffdbf54075d3161cc008944b1
Hccake <chengbohua@foxmail.com>
2021-10-16 17:38:23 +0800

    :art: 优化代码生成预览的弹窗展示效果

--- ballcat-codegen-frontend/src/views/gen/codegen/GeneratePreviewModal.vue
+++ ballcat-codegen-frontend/src/views/gen/codegen/GeneratePreviewModal.vue
@@ -1,7 +1,7 @@
 <template>
   <a-modal
     :title="title"
-    :width="950"
+    :width="1100"
     :visible="visible"
     :footer="null"
     :centered="true"
@@ -9,18 +9,28 @@
     class="preview-modal"
     @cancel="handleClose"
   >
-    <a-tabs>
-      <a-tab-pane v-for="(value, key) in data" :key="key" :tab="key">
-        <div style="padding-left: 8px; height: 800px; overflow-y: auto">
-          <pre><code class="hljs" v-html="highlightedCode(value,key)" /></pre>
-        </div>
-      </a-tab-pane>
-    </a-tabs>
+    <a-row>
+      <a-col :span="6" class="template-entry-tree-wrapper">
+        <a-directory-tree
+          :tree-data="templateEntryTree"
+          :show-icon="true"
+          :draggable="true"
+          @dblclick="ondblclick"
+        />
+      </a-col>
+      <a-col :span="18" class="template-content-wrapper">
+        <pre style="overflow: initial">
+          <code class="hljs" style="overflow: initial" v-html="codeContent" />
+        </pre>
+      </a-col>
+    </a-row>
   </a-modal>
 </template>
 <script>
 import hljs from 'highlight.js'
 import 'highlight.js/styles/github-gist.css'
+import { listToTree } from '@/utils/treeUtil'
+
 
 hljs.registerLanguage('java', require('highlight.js/lib/languages/java'))
 hljs.registerLanguage('xml', require('highlight.js/lib/languages/xml'))
@@ -36,7 +46,8 @@ export default {
       // 预览参数
       title: '代码预览',
       visible: false,
-      data: {},
+      templateEntryTree: [],
+      codeContent: '双击文件预览代码',
       defaultActiveKey: null
     }
   },
@@ -52,11 +63,24 @@ export default {
     },
     show(data) {
       this.visible = true
-      this.data = data
+      this.templateEntryTree = listToTree(data, 0, (treeNode, item) => {
+        treeNode.isLeaf = item.type === 2
+        treeNode.title = item.fileName
+      })
     },
     handleClose() {
       this.visible = false
-      this.data = {}
+      this.templateEntryTree = []
+      this.title = '代码预览'
+      this.codeContent = '双击文件预览代码'
+    },
+    ondblclick(e, node) {
+      // 非文件类型不加载
+      const entry = node.dataRef
+      if (entry.type === 2) {
+        this.codeContent = this.highlightedCode(entry.content, entry.fileName)
+        this.title = '代码预览 - ' + entry.fileName
+      }
     }
   }
 }
@@ -66,24 +90,44 @@ export default {
 .preview-modal >>> .ant-modal-header {
   padding: 8px 24px !important;
 }
+
 .preview-modal >>> .ant-modal-close-x {
   height: 40px;
   line-height: 40px;
 }
+
 .preview-modal >>> .ant-tabs-bar {
   margin: 0 !important;
 }
+
 .preview-modal >>> .ant-tabs-bar {
   margin: 0 !important;
 }
+
 .preview-modal >>> .ant-tabs-nav-container {
   line-height: 1;
 }
+
 .preview-modal >>> .ant-tabs-nav .ant-tabs-tab {
   margin: 0;
 }
 
 .preview-modal >>> .ant-tabs {
   line-height: 1.25;
+}
+
+.template-entry-tree-wrapper {
+  height: 650px;
+  overflow: auto;
+  border-right-style: solid;
+  border-right-width: 1px;
+  border-right-color: #dcdcdc;
+  background-color: #f5f5f5;
+}
+
+.template-content-wrapper {
+  padding-left: 8px;
+  height: 650px;
+  overflow: auto
 }
 </style>
