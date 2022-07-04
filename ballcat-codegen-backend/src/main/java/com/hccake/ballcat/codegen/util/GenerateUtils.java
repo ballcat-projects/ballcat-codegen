9da05ebdf11298774f2fbcfe6fdc7babd30beaee
mobaijun <50981510+mobaijun@users.noreply.github.com>
2022-07-04 22:04:10 +0800

    :fire: 删除无效代码

--- ballcat-codegen-backend/src/main/java/com/hccake/ballcat/codegen/util/GenerateUtils.java
+++ ballcat-codegen-backend/src/main/java/com/hccake/ballcat/codegen/util/GenerateUtils.java
@@ -67,5 +67,4 @@ public class GenerateUtils {
 	public static String underlineToCamel(String underlineStr) {
 		return WordUtils.capitalizeFully(underlineStr, new char[] { '_' }).replace("_", "");
 	}
-
 }
