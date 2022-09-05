6795c33e2d3de6c4dab65d827cca36b16e5cbd66
Hccake <chengbohua@foxmail.com>
2022-09-05 19:45:01 +0800

    :lipstick: 替换一个轴对称的 logo

--- ballcat-codegen-frontend/src/layouts/BasicHeader.vue
+++ ballcat-codegen-frontend/src/layouts/BasicHeader.vue
@@ -4,7 +4,7 @@
       <div class="ballcat-top-nav-header-main-left">
         <div class="ballcat-top-nav-header-logo">
           <a>
-            <img src="../assets/logo.svg" alt="logo" />
+            <img src="../assets/logo.png" alt="logo" />
             <h1>Ballcat Generator</h1>
           </a>
         </div>
@@ -106,9 +106,9 @@
       img,
       a > svg {
         display: inline-block;
-        height: 32px;
+        height: 28px;
         vertical-align: middle;
-        margin-bottom: 4px;
+        margin: 0 4px 4px 0;
       }
 
       h1 {
