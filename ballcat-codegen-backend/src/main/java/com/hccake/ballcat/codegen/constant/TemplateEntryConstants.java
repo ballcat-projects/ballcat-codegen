17d0b44a85805e2600208f3b14928907585b462b
Hccake <chengbohua@foxmail.com>
2022-10-20 23:04:02 +0800

    :art: code format

--- ballcat-codegen-backend/src/main/java/com/hccake/ballcat/codegen/constant/TemplateEntryConstants.java
+++ ballcat-codegen-backend/src/main/java/com/hccake/ballcat/codegen/constant/TemplateEntryConstants.java
@@ -4,7 +4,10 @@ package com.hccake.ballcat.codegen.constant;
  * @author hccake
  */
 public final class TemplateEntryConstants {
-	private TemplateEntryConstants() {}
+
+	private TemplateEntryConstants() {
+	}
 
 	public static final String TREE_ROOT_ID = "0";
+
 }
