f93daa6f8d81e1cea8fc1007f4a59c3578fd1e40
Hccake <chengbohua@foxmail.com>
2022-11-03 21:36:10 +0800

    :zap: 调整 axios 提交数组参数的格式化方式为 repeat

--- ballcat-codegen-frontend/src/utils/axios/index.ts
+++ ballcat-codegen-frontend/src/utils/axios/index.ts
@@ -2,11 +2,25 @@ import axios from 'axios'
 import type { AxiosResponse } from 'axios'
 import notification from 'ant-design-vue/es/notification'
 import type { BAxiosError } from '@/utils/axios/types'
+import qs from 'qs'
 
 // 创建 axios 实例
 const axiosInstance = axios.create({
   baseURL: import.meta.env.BASE_URL + 'api', // api base_url
-  timeout: 6000 // 请求超时时间
+  timeout: 6000, // 请求超时时间
+  paramsSerializer: function (params: any) {
+    return qs.stringify(params, {
+      // 数组的格式化方式为重复参数，例如 { a: ['1', '2']} => a=1&a=2
+      arrayFormat: 'repeat',
+      filter: (prefix: string, value: any) => {
+        // 空字符串不进行提交
+        if (typeof value == 'string' && value.length === 0) {
+          return
+        }
+        return value
+      }
+    })
+  }
 })
 
 // 请求失败处理函数
@@ -16,6 +30,7 @@ const onRejected = (error: BAxiosError) => {
     if (error.response.status === 403) {
       notification.error({
         message: 'Forbidden',
+        // @ts-ignore
         description: data.message
       })
       error.resolved = true
