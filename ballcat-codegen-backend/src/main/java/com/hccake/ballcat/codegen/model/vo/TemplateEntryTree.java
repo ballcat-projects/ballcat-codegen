c6fcaa1bd4fd055d0f8532e62874fee1962312ae
Hccake <chengbohua@foxmail.com>
2022-11-10 23:24:59 +0800

    :bug: 修复构建 TemplateEntryTree 时无法获取到 parentKey 的问题

--- ballcat-codegen-backend/src/main/java/com/hccake/ballcat/codegen/model/vo/TemplateEntryTree.java
+++ ballcat-codegen-backend/src/main/java/com/hccake/ballcat/codegen/model/vo/TemplateEntryTree.java
@@ -1,25 +1,23 @@
 package com.hccake.ballcat.codegen.model.vo;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.hccake.ballcat.codegen.constant.TemplateEntryTypeEnum;
-import com.hccake.ballcat.common.util.tree.AbstractIdTreeNode;
+import com.hccake.ballcat.common.util.tree.TreeNode;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
+import java.util.List;
 
 /**
  * @author Hccake 2020/6/21 17:59
  */
 @Data
-@EqualsAndHashCode(callSuper = true)
-public class TemplateEntryTree extends AbstractIdTreeNode<String> {
+public class TemplateEntryTree implements TreeNode<String> {
 
-	/**
-	 * ID
-	 */
-	@Schema(title = "ID")
 	private String id;
+
+	private String parentId;
+
+	private List<TemplateEntryTree> children;
 
 	/**
 	 * 模板组标识
@@ -50,5 +48,20 @@ public class TemplateEntryTree extends AbstractIdTreeNode<String> {
 	 */
 	@Schema(title = "模板引擎类型 1：velocity")
 	private Integer engineType;
+
+	@Override
+	public String getKey() {
+		return this.id;
+	}
+
+	@Override
+	public String getParentKey() {
+		return this.parentId;
+	}
+
+	@Override
+	public <T extends TreeNode<String>> void setChildren(List<T> list) {
+		this.children = (List<TemplateEntryTree>) list;
+	}
 
 }
