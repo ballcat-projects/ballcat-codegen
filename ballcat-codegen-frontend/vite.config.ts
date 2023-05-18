ef83124a67eb70925a18a3271c3d164dec30750f
Hccake <chengbohua@foxmail.com>
2023-05-18 17:03:41 +0800

    :fire: 移除 vite-plugin-style-import 插件

--- ballcat-codegen-frontend/vite.config.ts
+++ ballcat-codegen-frontend/vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
-import { createStyleImportPlugin, AndDesignVueResolve } from 'vite-plugin-style-import'
 import { antdvStyleDeps } from './src/utils/resolvers'
 import { fileURLToPath, URL } from 'url'
 import { splitVendorChunkPlugin } from 'vite'
 
 const serverAddress = 'http://localhost:7777'
 // const serverAddress = 'http://ballcat-admin:7777'
-// const serverAddress = 'http://codegen.ballcat.cn'
+//const serverAddress = 'http://codegen.ballcat.cn'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -21,25 +20,19 @@ export default defineConfig({
   base: '/',
   plugins: [
     vue(),
-    // 组件按需引入
+    // 按需加载 ant-design-vue 组件
     Components({
+      types: [
+        {
+          from: 'vue-router',
+          names: ['RouterLink', 'RouterView']
+        }
+      ],
       resolvers: [
         AntDesignVueResolver({
+          resolveIcons: true,
           importStyle: 'less'
         })
-      ]
-    }),
-    // 样式按需引入
-    createStyleImportPlugin({
-      resolves: [AndDesignVueResolve()],
-      libs: [
-        {
-          libraryName: 'ant-design-vue',
-          esModule: true,
-          resolveStyle: name => {
-            return `ant-design-vue/es/${name}/style/index`
-          }
-        }
       ]
     }),
     // 分块插件
