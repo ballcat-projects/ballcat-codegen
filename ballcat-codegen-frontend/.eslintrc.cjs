f1c78b0d0e057ce8e26a550070abe85d945b7fff
Hccake <chengbohua@foxmail.com>
2023-05-18 17:33:13 +0800

    :art: 调整 eslint 和 prettier 配置

--- ballcat-codegen-frontend/.eslintrc.cjs
+++ ballcat-codegen-frontend/.eslintrc.cjs
@@ -1,6 +1,7 @@
-const { defineConfig } = require('eslint-define-config')
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
 
-module.exports = defineConfig({
+module.exports = {
   root: true, // 当前配置为根配置，将不再从上级文件夹查找配置
   env: {
     browser: true,
@@ -9,22 +10,8 @@ module.exports = defineConfig({
   extends: [
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended'
-  ],
-  parser: 'vue-eslint-parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    ecmaFeatures: {
-      jsx: true
-    },
-    parser: '@typescript-eslint/parser',
-    sourceType: 'module'
-  },
-  plugins: [
-    'vue',
-    '@typescript-eslint',
-    'prettier'
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
   rules: {
     // 换行符检测关闭，避免跨系统时的冲突
@@ -39,5 +26,4 @@ module.exports = defineConfig({
     defineExpose: 'readonly',
     withDefaults: 'readonly'
   }
-
-})
+}
